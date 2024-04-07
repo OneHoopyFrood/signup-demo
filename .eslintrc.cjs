8e1c025a21064c52075310b1c34f998e2565df7d
Cole <3092695+OneHoopyFrood@users.noreply.github.com>
2024-04-07 14:18:08 -0500

    Configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["@typescript-eslint", "react"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+  ],
+  // rules: { },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+}
